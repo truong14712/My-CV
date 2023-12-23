9d22ec866d8ef98488744e12096f000222b5e080
Minh Trường <letruong147123@gmail.com>
2023-12-23 21:20:52 +0700

    feat: about me component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import AboutMe from "./components/AboutMe";
 import MyProjects from "./components/MyProjects";
 import Skill from "./components/Skill";
@@ -6,6 +6,7 @@ import ContactMe from "./components/ContactMe";
 import { useSelector, useDispatch } from "react-redux";
 const Home = () => {
   const [currentColor, setCurrentColor] = useState(getRandomColor);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const words = ["interest", "enjoy", "excitement", "passion"];
   const [text, setText] = useState(words[0]);
   const count = useSelector((state) => state.counter.value);
@@ -36,57 +37,53 @@ const Home = () => {
 
     return () => clearTimeout(timeoutId);
   }, [text, words]);
-  return (
-    <div className="container mx-auto 2xl:w-5/6 pt-[8rem] h-[10000px]">
-      <h1 className="text-[4rem] font-bold font-serif ">
-        Hello ✌🏼,
-        <br />
-        I&apos;m{" "}
-        <span
-          style={{
-            color: currentColor,
-            transition: "color 2s ease",
-          }}
-        >
-          Truong
-        </span>
-      </h1>
-      <p className="text-[1.3rem] space-x-2 mt-2">
-        A <span className="italic font-bold ">{text}</span> Front-end developer
-      </p>
-      <button
-        title="About me"
-        style={{
-          background: currentColor,
-          transition: "background 2s ease",
-          color: "#ffff",
-        }}
-        className="p-2 mt-[3rem] px-4 rounded-[24px] shadow-lg mb-[6rem]"
-      >
-        About me
-      </button>
+  // Tạo một tham chiếu cho component "AboutMe"
+  const aboutMeRef = useRef(null);
 
-      <AboutMe />
-      <Skill />
-      <MyProjects />
-      <ContactMe />
-      {/* <div>
-        <div>
-          <button
-            aria-label="Increment value"
-            onClick={() => dispatch(increment())}
+  // Hàm xử lý sự kiện khi nút được nhấp
+  const handleButtonAboutMe = () => {
+    // Cuộn đến component "AboutMe"
+    aboutMeRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+  return (
+    <div className="bg-gray-50">
+      <div className="container mx-auto 2xl:w-4/6 pt-[8rem] min-h-[100dvh]">
+        <h1 className="text-[4rem] font-bold">
+          Hello ✌🏼,
+          <br />
+          I&apos;m{" "}
+          <span
+            style={{
+              color: currentColor,
+              transition: "color 2s ease",
+            }}
           >
-            Increment
-          </button>
-          <span>{count}</span>
-          <button
-            aria-label="Decrement value"
-            onClick={() => dispatch(decrement())}
-          >
-            Decrement
-          </button>
+            Truong
+          </span>
+        </h1>
+        <p className="text-[1.3rem] space-x-2 mt-2">
+          A <span className="italic font-bold ">{text}</span> Front-end
+          developer
+        </p>
+        <button
+          title="About me"
+          style={{
+            background: currentColor,
+            transition: "background 2s ease",
+            color: "#ffff",
+          }}
+          className="p-2 mt-[3rem] px-4 rounded-[24px] shadow-lg mb-[6rem]"
+          onClick={handleButtonAboutMe}
+        >
+          About me
+        </button>
+        <div ref={aboutMeRef}>
+          <AboutMe />
         </div>
-      </div> */}
+        <Skill />
+        <MyProjects />
+        <ContactMe />
+      </div>
     </div>
   );
 };
--- src/pages/components/AboutMe.jsx
+++ src/pages/components/AboutMe.jsx
@@ -1,5 +1,39 @@
 const AboutMe = () => {
-  return <div className="mt-[10rem]">AboutMe</div>;
+  return (
+    <div className="pt-[60px] mt-[20%]">
+      <div className="text-2xl font-semibold text-center">About me</div>
+      <div className="mt-1 text-xl text-center">
+        if you&apos;re <span className="italic font-semibold">wondering</span>{" "}
+        who <span className="italic font-semibold">I am...</span>
+      </div>
+      <div className="mt-[24px]">
+        <div className="text-lg">
+          Hi, my name is <span>Truong</span>, i&apos;m 21 years old. I got a
+          college degree at FPT Polytechnic College in Hanoi (2021-2023).
+          Besides, i am really passionate about <span>Javascript</span> and
+          <span> Web Development</span>.
+        </div>
+        <br />
+        <div className="text-lg">
+          I started learning web programming in 2021. Before that, I had a
+          background in C / C ++ programming so getting access to javascript is
+          not difficult. I have been in love with javascript since I did not
+          know it, I like it. I searched many different sources to learn about
+          this language for example:{" "}
+          <span>
+            <span className="italic underline"> Developer mozilla</span>,{" "}
+            <span className="italic underline">You Don&apos;t Know JS</span>
+          </span>
+          ....
+        </div>
+        <br />
+        <div className="text-lg">
+          I want to be a good person in the field that I have chosen so I tried
+          a lot, worked hard.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutMe;
