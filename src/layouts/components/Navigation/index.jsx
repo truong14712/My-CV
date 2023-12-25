b4fc16551bfc66bab7842271f06b777fbca470a4
Minh Trường <letruong147123@gmail.com>
2023-12-25 09:54:50 +0700

    fix: navigation and add custom scroll website

--- src/layouts/components/Navigation/index.jsx
+++ src/layouts/components/Navigation/index.jsx
@@ -1,24 +1,33 @@
 import { Link, useLocation } from "react-router-dom";
 import TabNavigation from "../../../components/TabNavigation";
+
 const Navigation = () => {
   const location = useLocation();
   const currentPath = location.pathname;
+
   return (
     <div className="relative ">
       <div className="fixed transform -translate-x-1/2 bottom-5 2xl:w-1/6 left-1/2 flex items-center">
-        <ul className="flex items-center bg-white border drop-shadow-lg rounded-[20px] px-4 cursor-pointer opacity-95 z-10 justify-center py-2">
+        <ul className="flex items-center bg-white border drop-shadow-lg rounded-[16px] px-4 cursor-pointer opacity-95 z-10 justify-center py-2">
           {TabNavigation.map((item, index) => {
             return (
-              <li
+              <Link
                 key={index}
-                className={currentPath == item.path && "text-blue-500"}
+                className={
+                  currentPath == item.path &&
+                  `bg-gradient-to-r from-sky-500 to-indigo-500  text-white rounded-[16px] ease-linear duration-300`
+                }
                 title={item.title}
                 style={{
-                  padding: "0 20px",
+                  padding: "4px 20px",
+                  borderRadius: "16px",
+                  color: currentPath == item.path ? "#fff" : "#000",
+                  
                 }}
+                to={item.path}
               >
-                <Link to={item.path}>{item.icon}</Link>
-              </li>
+                <span>{item.icon}</span>
+              </Link>
             );
           })}
         </ul>
